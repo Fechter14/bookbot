4352bbb19734eecdb0b2e4bf5ed164e05b12d188
Fechter14 <trevsinz@yahoo.com>
2024-06-26 18:42:28 -0400

    add report creator

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ def main():
     word_count = count_words(text)
     char_counts = count_characters(text)
     #print(text)
-    print(f"There are {word_count} words in the above text.")
-    print(char_counts)
+    #print(f"There are {word_count} words in the above text.")
+    #print(char_counts)
+    create_report(book_path, word_count, char_counts)
 
 def get_book_text(path):    
     with open(path) as f:
@@ -26,5 +27,28 @@ def count_characters(text):
             char_counts[char] = 1
 
     return char_counts
+
+def create_report(book_path, word_count, char_counts):
+    alpha_list = []
+    
+    def sort_on(dict):
+        return dict["count"]
+
+    for char in char_counts.keys():
+        if char.isalpha():
+            new_dict = {"letter": char, "count": char_counts[char]}
+            alpha_list.append(new_dict)
+    
+    alpha_list.sort(reverse=True, key=sort_on)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"The book contained {word_count} words.")
+    print("")
+
+    for item in alpha_list:
+        print(f"The letter \'{item['letter']}\' was found {item['count']} times")
+
+    print("--- End report ---")
+
 
 main()
